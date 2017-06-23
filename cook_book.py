fe86d21f8f49d04478a6d7dd0195111fa8255fe7
kirill.smolov <ksmolov@gmail.com>
2017-06-23 11:06:54 +0300

    Обновлена программа, с чтением из файла с произвольной структурой данных. Учтены комментарии.

--- cook_book.py
+++ cook_book.py
@@ -11,15 +11,15 @@ def read_cook_book_from_file():
             for i in range(number_of_ingredient):
                 ingred = cb.readline()
                 ingred_list = ingred.split()
-                for k, item in enumerate(ingred_list):
-                    if item == '|':
-                        ingred_list.pop(k)
-                ingredients_for_dish.append({'ingridient_name' : ingred_list[0].lower(), 'quontity' : int(ingred_list[1]),\
+                ingred_list = [k for k in ingred_list if k != '|']
+                # ingred_list = list(filter(lambda k: k != '|', ingred_list))
+                ingredients_for_dish.append({'ingridient_name' : ingred_list[0].lower(),\
+                                             'quantity' : int(ingred_list[1]),\
                                              'measure' : ingred_list[2].lower()})
-                cook_book[dish_name] = ingredients_for_dish
-
+            cook_book[dish_name] = ingredients_for_dish
             cb.readline()
     return cook_book
+
 
 def get_shop_list_by_dishes(dishes, person_count):
     cook_book = read_cook_book_from_file()
@@ -27,17 +27,17 @@ def get_shop_list_by_dishes(dishes, person_count):
     for dish in dishes:
         for ingredient in cook_book[dish]:
             new_shop_list_item = dict(ingredient)
-            new_shop_list_item['quontity'] *= person_count
+            new_shop_list_item['quantity'] *= person_count
             if new_shop_list_item['ingridient_name'] not in shop_list:
                 shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quontity'] += new_shop_list_item['quontity']
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
 
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{ingridient_name} {quontity} {measure}'.format(**shop_list_item))
+        print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
 
 def create_shop_list():
     dishes = input('Введите блюдо в расчете на одного человека:').lower().split(', ')
