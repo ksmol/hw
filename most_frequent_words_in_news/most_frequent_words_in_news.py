be6cda19aff14792088aeb3d3aaa3e10273dd8b3
Kirill Smolov <ksmolov@gmail.com>
2017-06-25 22:42:33 +0300

    rename dir

---
+++ most_frequent_words_in_news/most_frequent_words_in_news.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+def input_file_name():
+    file_name = input('Введите имя файла')
+    file_extention = '.json'
+    file = file_name + file_extention
+    print(file)
+    return file
+
+def read_cook_book_from_file():
+    from pprint import pprint
+    import  json
+    with open(input_file_name(), 'r', encoding='utf-8') as cb:
+     cook_book = json.load(cb)
+    pprint(cook_book)
+    return cook_book
+
+read_cook_book_from_file()
