a974e45967e9335c71cf76476c0bceef23009208
kirill.smolov <ksmolov@gmail.com>
2017-06-26 17:16:52 +0300

    Чтение файлов с новостями

--- most_frequent_words_in_news/most_frequent_words_in_news.py
+++ most_frequent_words_in_news/most_frequent_words_in_news.py
@@ -1,22 +1,35 @@
 #-*- coding: utf-8 -*-
 
 def input_file_name():
-    file_name = input('Введите имя файла')
+    file_name = input('Введите имя файла в формате.json:\n')
     file_extention = '.json'
     file = file_name + file_extention
     print(file)
     return file
 
-def identify_and_decode_file_encoding(file_name)
-    file_name = input_file_name()
+def identify_and_decode_file_encoding(file_name):
     import chardet
+    with open(file_name, 'rb') as file:
+        data = file.read()
+        detected_encoding = chardet.detect(data)
+        file_encoding = detected_encoding['encoding']
+        print(detected_encoding)
+    return file_encoding
 
-def read_cook_book_from_file():
-    from pprint import pprint
-    import  json
-    with open(input_file_name(), 'r', encoding='utf-8') as cb:
-     cook_book = json.load(cb)
-    pprint(cook_book)
-    return cook_book
+def read_news_from_file():
+    file_name = input_file_name()
+    import json
+    with open(file_name, 'r', encoding=identify_and_decode_file_encoding(file_name)) as nf:
+       news_file = json.load(nf)
+    return news_file
 
-read_cook_book_from_file()
+
+news_file = read_news_from_file()
+text_from_all_news = []
+for news_index in news_file['rss']['channel']['items']:
+    print(news_index)
+    # words = news_file['rss']['channel']['items'][news_num]['description'].split()
+    # text_from_all_news += words
+
+# text_from_all_news = [w.lower() for w in text_from_all_news if len(w)>=6]
+# print(text_from_all_news)
