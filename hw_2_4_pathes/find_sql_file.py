9bd32ba1c17e12f6982e9ad94a19112a4400a48f
kirill.smolov <ksmolov@gmail.com>
2017-06-28 17:18:55 +0300

    -

---
+++ hw_2_4_pathes/find_sql_file.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+
+def input_word_for_search():
+    word_for_search = input().upper()
+    return word_for_search
+import os
+os.listdir(os.path.join(os.path.abspath('Migrations'))))
