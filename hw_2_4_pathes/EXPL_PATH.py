a7e2180cfafc1231f538fbd06683f0aba6f464cb
kirill.smolov <ksmolov@gmail.com>
2017-06-29 15:30:33 +0300

    -

--- hw_2_4_pathes/EXPL_PATH.py
+++ hw_2_4_pathes/EXPL_PATH.py
@@ -1,3 +1,6 @@
+import os
 list_of_all_sql_files = [files for files in
                          os.listdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations')) if
                          '.sql' in files]
+for file_name in list_of_all_sql_files:
+    print(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations', file_name))
