9c5d29a6352326358ae3a8ea91c6f39fd170193a
kirill.smolov <ksmolov@gmail.com>
2017-06-29 14:41:27 +0300

    Renamed file.

--- hw_2_4_pathes/search_in_files.py
+++ hw_2_4_pathes/search_in_files.py
@@ -1,61 +1,56 @@
 #-*- coding: utf-8 -*-
 
-def input_command():
-    command = input().lower()
-    return command
+def enter_data():
+    data = input()
+    return data
 
-def input_word_for_search():
-    word_for_search = input().upper()
-    return word_for_search
-
-def get_list_of_sql_files():
+def get_list_of_all_sql_files_at_dir():
     import os
-    list_dir = [files for files in os.listdir(os.path.abspath('Migrations')) if '.sql' in files]
-    return list_dir
+    list_of_all_sql_files = [files for files in os.listdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations')) if '.sql' in files]
+    return list_of_all_sql_files
 
-def find_file_by_keyword(keyword, list_of_files):
+def find_file_by_keyword(keyword, list_of_all_sql_files):
     import os
     file_counter = 0
-    file_list_iterations = []
-    for file_name in list_of_files:
-        with open(os.path.join(os.path.abspath('Migrations'),file_name), 'r') as f:
+    list_of_found_files = []
+    for file_name in list_of_all_sql_files:
+        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations', file_name), 'r') as f:
             data = f.read()
             if keyword in data:
                 file_counter += 1
-                file_list_iterations.append(file_name)
+                list_of_found_files.append(file_name)
 
-    return file_counter, file_list_iterations
+    return file_counter, list_of_found_files
 
 def dialog_window():
     while True:
         print('Введите команду из списка:\n\
-            e - enter a word to search and show list of found files\n\
+            e - enter (ввод строки, по которой будет осуществлен поиск)\n\
             q - quit (прекратить выполнение программы)')
-
-        command = input_command()
+        command = enter_data().lower()
         if command == 'e':
-            list_of_files = get_list_of_sql_files()
+            list_of_files = get_list_of_all_sql_files_at_dir()
             i = True
             while i == True:
                 print('Введите слово для поиска:')
-                keyword = input_word_for_search()
+                keyword = enter_data().upper()
                 results = find_file_by_keyword(keyword, list_of_files)
                 list_of_files = results[1]
-                print('\nCлово "{}" найдено в {} следующих файлах:\n'.format(keyword, results[0]))
+                print('\nСтрока "{}" найдена в {} следующих файлах:\n'.format(keyword, results[0]))
                 for file_name in list_of_files:
                     print(file_name)
                 print('\nВы хотите продолжить поиск в этом списке?(y/n)')
-                yn_command = input_command()
+                yn_command = enter_data()
                 if yn_command == 'y':
                     i = True
                 else:
                     i = False
                     print('\nПосле ввода команды "e" вы сможете начать поиск по файлам заново.\n')
-
         elif command == 'q':
             exit()
         else:
             print('\nВы ввели неправильную команду\n')
+
 
 if __name__ == '__main__':
     dialog_window()
