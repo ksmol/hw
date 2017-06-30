7ba3f27b58a4853acd0e94422331f6511be2cb16
kirill.smolov <ksmolov@gmail.com>
2017-06-30 17:06:41 +0300

    -

--- hw_2_5_external_programs/image_converter(ver_1).py
+++ hw_2_5_external_programs/image_converter(ver_1).py
@@ -3,34 +3,40 @@
 import os
 import subprocess
 
+def get_path_to_work_dir():
+    path_to_work_dir = os.path.abspath(os.path.dirname(__file__))
+    return path_to_work_dir
+
+
 def get_image_list():
     list_of_image_files = [image for image
-                      in os.listdir(os.path.join( os.path.dirname(__file__), 'Source'))
+                      in os.listdir(os.path.join(get_path_to_work_dir(), 'Source'))
                       if '.jpg' in image]
     return list_of_image_files
 
 def create_new_folder_at_working_dir(folder_name):
-    if os.path.isdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), folder_name)) == False:
-        os.mkdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), folder_name))
+    if os.path.isdir(os.path.join(get_path_to_work_dir(), folder_name)) == False:
+        os.mkdir(os.path.join(get_path_to_work_dir(), folder_name))
 
-        print('\nПапка с именем "{}" создана в директории "{}"'
-                .format(folder_name, os.path.abspath(os.path.dirname(__file__))))
+        print('\nПапка с именем "{}" создана в директории "{}"'.format(folder_name, get_path_to_work_dir()))
 
 
 def convert_all_images(source_folder, name_of_output_folder):
     create_new_folder_at_working_dir(name_of_output_folder)
     list_of_image_files = get_image_list()
+
     for image_file_name in list_of_image_files:
-        subprocess.check_call(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'convert ')
-                + os.path.join(os.path.abspath(os.path.dirname(__file__)), source_folder, image_file_name)
+        subprocess.check_call(os.path.join(get_path_to_work_dir(), 'convert ')
+                + os.path.join(get_path_to_work_dir(), source_folder, image_file_name)
                 + ' -resize 200x '
-                + os.path.join(os.path.abspath(os.path.dirname(__file__)), name_of_output_folder, image_file_name))
+                + os.path.join(get_path_to_work_dir(), name_of_output_folder, image_file_name))
+
     print('\nИзображения успешно конвертированы и находятся по адресу: {}'
-                .format(os.path.join(os.path.abspath(os.path.dirname(__file__)), name_of_output_folder)))
+          .format(os.path.join(get_path_to_work_dir(), name_of_output_folder)))
 
 def dialog_window():
     source_folder = input('Введите имя папки, в которой вы хотите конвертировать изображения:\n')
-    if os.path.isdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), source_folder)) == False:
+    if os.path.isdir(os.path.join(get_path_to_work_dir(), source_folder)) == False:
         print('\nУказанной папки нет в рабочей директории.')
         exit(1)
     else:
--- hw_2_5_external_programs/image_converter(ver_2).py
+++ hw_2_5_external_programs/image_converter(ver_2).py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 
+def get_path_to_
 def get_image_list():
     list_of_image_files = [image for image
                       in os.listdir(os.path.join( os.path.dirname(__file__), 'Source'))
@@ -11,6 +12,7 @@ def get_image_list():
 
 
 def create_new_folder_at_working_dir(folder_name):
+    path_to_work_dir =
     if os.path.isdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), folder_name)) == False:
         os.mkdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), folder_name))
 
@@ -18,9 +20,9 @@ def create_new_folder_at_working_dir(folder_name):
                 .format(folder_name, os.path.abspath(os.path.dirname(__file__))))
 
 
-def convert_all_images(source_folder, name_of_output_folder):
+def convert_all_images(image_file_name, source_folder, name_of_output_folder):
     create_new_folder_at_working_dir(name_of_output_folder)
-        subprocess.check_call(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'convert ')
+    subprocess.check_call(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'convert ')
                 + os.path.join(os.path.abspath(os.path.dirname(__file__)), source_folder, image_file_name)
                 + ' -resize 200x '
                 + os.path.join(os.path.abspath(os.path.dirname(__file__)), name_of_output_folder, image_file_name))
@@ -34,7 +36,8 @@ def dialog_window():
         exit(1)
     else:
         output_folder = input('\nВведите имя папки, в которую будут сохранены изображения:\n')
-        convert_all_images(source_folder, output_folder)
+        list = get_image_list()
+        map(convert_all_images, list, source_folder, output_folder)
 
 if __name__ == '__main__':
     dialog_window()
