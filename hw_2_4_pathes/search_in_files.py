189a0ca03dbde194ddc88ea9f1ae155a62ffaf0c
kirill.smolov <ksmolov@gmail.com>
2017-07-03 11:58:52 +0300

    Учтены комментарии по работе.

--- hw_2_4_pathes/search_in_files.py
+++ hw_2_4_pathes/search_in_files.py
@@ -2,23 +2,23 @@
 
 import os
 
+def return_path_to_file(file_name):
+    path_to_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
+    return path_to_file
+
 def get_list_of_all_sql_files_at_dir():
-    list_of_all_sql_files = [files for files in
-                             os.listdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations'))
-                             if '.sql' in files]
+    list_of_all_sql_files = [file for file in os.listdir(return_path_to_file('Migrations'))
+                             if file.endswith('.sql')]
     return list_of_all_sql_files
 
 def find_file_by_keyword(keyword, list_of_all_sql_files):
-    file_counter = 0
     list_of_found_files = []
     for file_name in list_of_all_sql_files:
-        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Migrations', file_name), 'r') as f:
+        with open(os.path.join(return_path_to_file('Migrations'), file_name), 'r') as f:
             data = f.read()
             if keyword in data:
-                file_counter += 1
                 list_of_found_files.append(file_name)
-
-    return file_counter, list_of_found_files
+    return len(list_of_found_files), list_of_found_files
 
 def dialog_window():
     while True:
@@ -28,21 +28,21 @@ def dialog_window():
         command = input().lower()
         if command == 'e':
             list_of_files = get_list_of_all_sql_files_at_dir()
-            i = True
-            while i == True:
+            main_state = True
+            while main_state:
                 print('Введите слово для поиска:')
                 keyword = input().upper()
                 results = find_file_by_keyword(keyword, list_of_files)
                 list_of_files = results[1]
                 print('\nСтрока "{}" найдена в {} следующих файлах:\n'.format(keyword, results[0]))
-                for file_name in list_of_files:
-                    print(file_name)
+                if list_of_files:
+                    print('\n'.join(list_of_files))
                 print('\nВы хотите продолжить поиск в этом списке?(y/n)')
                 yn_command = input()
                 if yn_command == 'y':
-                    i = True
+                     continue
                 else:
-                    i = False
+                    main_state = False
                     print('\nПосле ввода команды "e" вы сможете начать поиск по файлам заново.\n')
         elif command == 'q':
             exit()
