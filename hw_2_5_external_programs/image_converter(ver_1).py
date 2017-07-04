cff5e726d19d59cff977ee74f514ee79ed602278
kirill.smolov <ksmolov@gmail.com>
2017-07-04 10:44:18 +0300

    -

--- hw_2_5_external_programs/image_converter(ver_1).py
+++ hw_2_5_external_programs/image_converter(ver_1).py
@@ -3,34 +3,34 @@
 import os
 import subprocess
 
-def get_path_to_folder(folder_name):
-    path_to_work_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), folder_name)
+def get_path_to_file(file_name):
+    path_to_work_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
     return path_to_work_dir
 
 
 def get_image_list(source_folder):
-    list_of_image_files = [image for image in os.listdir(get_path_to_folder(source_folder)) if '.jpg' in image]
+    list_of_image_files = [image for image in os.listdir(get_path_to_file(source_folder)) if '.jpg' in image]
     return list_of_image_files
 
-def create_new_folder(folder_name):
-    if os.path.isdir(get_path_to_folder(folder_name)) == False:
-        os.mkdir(get_path_to_folder(folder_name))
-        print('\nПапка с именем "{}" создана в директории "{}"'.format(folder_name, get_path_to_folder('')))
+def create_new_folder(file_name):
+    if os.path.isdir(get_path_to_file(file_name)) == False:
+        os.mkdir(get_path_to_file(file_name))
+        print('\nПапка с именем "{}" создана в директории "{}"'.format(file_name, get_path_to_file('')))
 
 
 def convert_all_images(source_folder, output_folder):
     create_new_folder(output_folder)
     list_of_image_files = get_image_list(source_folder)
     for image_file_name in list_of_image_files:
-        subprocess.check_call(get_path_to_folder('convert ')
-                              + os.path.join(get_path_to_folder(source_folder), image_file_name)
+        subprocess.check_call(get_path_to_file('convert ')
+                              + os.path.join(get_path_to_file(source_folder), image_file_name)
                               + ' -resize 200x '
-                              + os.path.join(get_path_to_folder(output_folder), image_file_name))
-    print('\nИзображения успешно конвертированы и находятся по адресу: {}'.format(get_path_to_folder(output_folder)))
+                              + os.path.join(get_path_to_file(output_folder), image_file_name))
+    print('\nИзображения успешно конвертированы и находятся по адресу: {}'.format(get_path_to_file(output_folder)))
 
 def dialog_window():
     source_folder = input('Введите имя папки, в которой вы хотите конвертировать изображения:\n')
-    if os.path.isdir(get_path_to_folder(source_folder)) == False:
+    if os.path.isdir(get_path_to_file(source_folder)) == False:
         print('\nУказанной папки нет в рабочей директории.')
         exit(1)
     else:
