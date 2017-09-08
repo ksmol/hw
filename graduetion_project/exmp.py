496224922128af4b533273f8fcb71a90716e5cf9
kirill.smolov <ksmolov@gmail.com>
2017-09-08 17:23:46 +0300

    исправлены замечания

--- graduetion_project/exmp.py
+++ graduetion_project/exmp.py
@@ -1,11 +1,11 @@
-a = [x for x in range(10)]
-print(a)
-b = [x for x in range(11, 20)]
-print(b)
-c = [a, b]
-print(c)
-for ind in c:
-    for i in ind:
-        print(i)
-d = [i for ind in c for i in ind]
-print(d)
+def decorator(func):
+    def wrapper(*kwargs):
+        for i in range(3):
+            func(*kwargs)
+    return wrapper
+
+@decorator
+def sing(line):
+    print(line)
+
+sing('tgsfg')
--- graduetion_project/spyware.py
+++ graduetion_project/spyware.py
@@ -5,52 +5,45 @@ import time
 import json
 import os
 
+
 VERSION = '5.68'
 TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
 
 
-def get_user_id(user_id):
-    params = {
-        'user_ids': user_id,
-        'access_token': TOKEN,
-        'fields': 'screen_name',
-        'v': VERSION
-    }
-    response = requests.get('https://api.vk.com/method/users.get', params)
-    return response.json()['response'][0]['id']
-
-
-def get_ids_of_friends_or_groups(user_id,
-                                 method='friends.get',
-                                 fields=None,
-                                 extended=0,
-                                 count=1000):
-    params = {
-        'user_id': user_id,
-        'access_token': TOKEN,
-        'fields': fields,
-        'extended': extended,
-        'count': count,
-        'v': VERSION
-    }
+def call_vk_api(method='users.get', **params):
+    response = requests.get('https://api.vk.com/method/{}'.format(method), params)
+    time.sleep(0.5)
+    print('.')
     try:
-        response = requests.get('https://api.vk.com/method/{}'.format(method), params)
-        time.sleep(0.5)
-        print('.')
         return response.json()['response']
     except KeyError:
-        print('Пользователь {} был заблокирован или удалил свою страницу.'
-              .format(user_id))
-        return {'items': []}
+        err_response = response.json()['error']
+        skipped_errors_nums = [18, 24, 113]
+        if err_response['error_code'] in skipped_errors_nums:
+            print('Ошибка VK Api №{} - "{}"'.format(err_response['error_code'],
+                                                    err_response['error_msg']))
+            return {'items': []}
+        else:
+            print('Ошибка VK Api №{} - "{}"'.format(err_response['error_code'],
+                                                    err_response['error_msg']))
+            exit(1)
+
+#
+# def friends_get(call_vk_api):
+#     def give_arg_to_api(params):
+#         params = {user_id:'user_id'{}
+#         call_vk_api(method='friends.get')
+#     return call_vk_api
 
 
 def get_all_friends_groups(user_friends_ids):
     all_friends_groups = []
     for friend_id in user_friends_ids:
-        users_groups = get_ids_of_friends_or_groups(friend_id,
-                                                    method='groups.get')
-        for group_id in users_groups['items']:
-            all_friends_groups.append(group_id)
+        users_groups = call_vk_api(method='groups.get',
+                                   user_id=friend_id,
+                                   access_token=TOKEN,
+                                   v=VERSION)
+        all_friends_groups.extend(users_groups['items'])
     return all_friends_groups
 
 
@@ -70,21 +63,21 @@ def get_unique_groups_info(user_groups, friend_groups):
 
 
 def get_path_to_work_dir(file_name=''):
-    path_to_work_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
+    path_to_work_dir = os.path.join(os.path.abspath(file_name))
     return path_to_work_dir
 
 
 def create_new_folder(file_name='Results'):
-    if not os.path.isdir(get_path_to_work_dir(file_name)):
-        os.mkdir(get_path_to_work_dir(file_name))
-        print('\nПапка с именем "{}" создана в директории "{}"'
-              .format(file_name, get_path_to_work_dir('')))
+        path_to_dir = get_path_to_work_dir(file_name)
+        if not os.path.exists(path_to_dir):
+            os.makedirs(get_path_to_work_dir(file_name), exist_ok=True)
+            print('\nПапка с именем "{}" создана в директории "{}"'
+                  .format(file_name, get_path_to_work_dir('')))
 
 
 def record_data_to_json_file(data, path, file_name, encoding='utf=8'):
-    with open(os.path.join(os.path.abspath(path), file_name),
-              'w',
-              encoding=encoding) as f:
+    path_to_dump_file = os.path.join(path, file_name)
+    with open(path_to_dump_file, 'w', encoding=encoding) as f:
         json.dump(data,
                   f,
                   sort_keys=False,
@@ -100,22 +93,33 @@ def dialog_window():
         if command == 'ug':
             input_id = input('\nПожалуйста, введите имя пользователя,'
                              '\nчтобы выгрузить информацию о его уникальных группах\n')
-            current_user_id = get_user_id(input_id)
-            user_friends_ids = get_ids_of_friends_or_groups(current_user_id)
-            user_groups = get_ids_of_friends_or_groups(current_user_id,
-                                                       method='groups.get',
-                                                       fields='members_count',
-                                                       extended=1)
+            current_user_id = call_vk_api(user_ids=input_id,
+                                          access_token=TOKEN,
+                                          v=VERSION)
+            user_friends_ids = call_vk_api(method='friends.get',
+                                           user_id=current_user_id[0]['id'],
+                                           access_token=TOKEN,
+                                           v=VERSION)
+            user_groups = call_vk_api(method='groups.get',
+                                      user_id=current_user_id[0]['id'],
+                                      access_token=TOKEN,
+                                      v=VERSION,
+                                      fields='members_count',
+                                      extended=1,
+                                      count=1000)
             friend_groups = get_all_friends_groups(user_friends_ids['items'])
             groups_info = get_unique_groups_info(user_groups, friend_groups)
+            create_new_folder('Results')
+            path_to_work_dir = get_path_to_work_dir('Results')
             record_data_to_json_file(groups_info,
-                                     get_path_to_work_dir('Results'),
+                                     path_to_work_dir,
                                      'groups.json')
-            exit(0)
+            break
         elif command == 'q':
-            exit(0)
+            break
         else:
             print('\nВы ввели неправильную команду\n')
+
 
 if __name__ == '__main__':
     dialog_window()
